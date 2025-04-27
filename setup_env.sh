08b333ad397cf116789ab88daa48953a7deff038
Steven Yu <2322740290qq.com>
2025-04-26 20:14:23 -0400

    update

--- setup_env.sh
+++ setup_env.sh
@@ -1,16 +1,23 @@
-module load cuda12.2/
-module load mamba
+# module load cuda12.2/
+# module load mamba
+# mamba init
+# source /burg/home/yy3448/.bashrc
+module load cuda
 mamba init
-source /burg/home/yy3448/.bashrc
+source ~/.bashrc
 
 if mamba env list | grep -q 'peak'; then
     echo "Environment 'peak' found. Activating it..."
     mamba activate peak
 else
     echo "Environment 'peak' not found. Creating it..."
-    mamba create -n peak
-    mamba activate peak
+    mamba create -n peak_traj python=3.11 --yes
+    mamba activate peak_traj
 fi
 
-pip install scanpy pandas numpy
+pip install pandas numpy==1.26 anndata muon scikit-learn umap-learn
+pip install pyranges gffutils pybedtools
+pip install scipy h5py tables
+pip install gtfparse intervaltree
+
 mamba install ipykernel ipywidgets scanpy biotite matplotlib seaborn --yes
